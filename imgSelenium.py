4729b9aa65dcaa5dfd4306b90e7abbc149a75c43
eundongg <remy08@naver.com>
2023-03-20 18:39:53 +0900

    Update imgSelenium.py

--- imgSelenium.py
+++ imgSelenium.py
@@ -1,29 +1,25 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
 import time
 import urllib.request
+import os
 
 options = webdriver.ChromeOptions()
-options.add_experimental_option('excludeSwitches',['enable-logging'])
+options.add_experimental_option("excludeSwitches", ["enable-logging"])
 driver = webdriver.Chrome(options=options)
-driver.get("https://www.google.co.kr/imghp?hl=ko&ogbl")
+driver.get("https://www.google.co.kr/imghp?hl=ko&tab=wi&authuser=0&ogbl")
 elem = driver.find_element_by_name("q")
-elem.send_keys("댕댕이")
+elem.send_keys("고양이")
 elem.send_keys(Keys.RETURN)
 
 SCROLL_PAUSE_TIME = 1
-
 # Get scroll height
 last_height = driver.execute_script("return document.body.scrollHeight")
-
 while True:
     # Scroll down to bottom
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-
     # Wait to load page
     time.sleep(SCROLL_PAUSE_TIME)
-
     # Calculate new scroll height and compare with last scroll height
     new_height = driver.execute_script("return document.body.scrollHeight")
     if new_height == last_height:
@@ -33,16 +29,19 @@ while True:
             break
     last_height = new_height
 
-
-count =1
 images = driver.find_elements_by_css_selector(".rg_i.Q4LuWd")
+count = 1
+
 for image in images:
-    try: 
+    try:
         image.click()
         time.sleep(2)
-        imgURL = driver.find_element_by_xpath("/html/body/div[2]/c-wiz/div[3]/div[2]/div[3]/div/div/div[3]/div[2]/c-wiz/div[1]/div[1]/div/div[2]/a/img").get_attribute("src")
-        urllib.request.urlretrieve(imgURL,str(count)+".jpg")
-        count = count +1
+        imgUrl = driver.find_element_by_xpath('//*[@id="Sva75c"]/div[2]/div/div[2]/div[2]/div[2]/c-wiz/div/div[1]/div[2]/div[2]/div/a/img').get_attribute("src")
+        opener=urllib.request.build_opener()
+        opener.addheaders=[('User-Agent','Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36')]
+        urllib.request.install_opener(opener)
+        urllib.request.urlretrieve(imgUrl, str(count) + ".jpg")
+        count = count + 1
     except:
         pass
 
